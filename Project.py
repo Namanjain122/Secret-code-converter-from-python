e5d0b4608d862a42170a9f055e2e9867163253d4
Naman Jain <161619333+Namanjain122@users.noreply.github.com>
2024-03-05 15:12:51 +0530

    Add files via upload

---
+++ Project.py
@@ -0,0 +1,43 @@
+from tkinter import *
+from PIL import Image,ImageTk
+def Secret_code():
+    input_message = entry.get() #used to take input from user
+    words = input_message.split(" ")
+    nwords = []
+    for word in words:
+        if len(word) >= 3:
+            r1 = "abc"
+            r2 = "efg"
+            new_string = r1 + word[1:] + word[0] + r2
+            nwords.append(new_string)
+        else:
+            nwords.append(word[::-1])
+
+    output_label.config(text=" ".join(nwords))  
+
+
+#creating window
+window = Tk()
+window.geometry("1000x1000")
+window.title("Secret code")
+
+# Create input entry widget
+entry_label = Label(window,text="Enter the message:",bg="red",fg="white")
+entry_label.pack()
+entry = Entry(window)
+entry.pack()
+
+convert_button = Button(window, text="Convert",bg="blue",fg="white",command=Secret_code)
+convert_button.pack()
+
+# Create output label widget
+output_label = Label(window, text="")
+output_label.pack()
+
+#adding  background image 
+Image = Image.open("Naman.jpg")
+photo = ImageTk.PhotoImage(Image)
+Naman_Lable = Label(image=photo)
+Naman_Lable.pack()
+# Start the tkinter event loop
+window.mainloop()
